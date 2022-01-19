070024a9f5d4db709fe55dc7710c4d1690fb935b
said jabre <saidjabre@gmail.com>
2022-01-19 22:58:59 +0200

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+function ValidateEntries (){
+    var name= document.getElementById("name").value;
+    var surname= document.getElementById("surname").value;
+    var city= document.getElementById("city").value;
+    alert ("Nom: "+name+"\n\nPrenom: "+surname+"\n\nVille: "+city);
+}
