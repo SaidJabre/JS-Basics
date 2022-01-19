ec42dc268ff2b9c32b044065e539a78532e0b7ed
said jabre <saidjabre@gmail.com>
2022-01-19 22:59:46 +0200

    Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,6 @@
+function GetRemainder (){
+    var fnumber= document.getElementById("first_number").value;
+    var snumber= document.getElementById("second_number").value;
+    var result = fnumber % snumber;
+    alert("Result: " + result);
+}
