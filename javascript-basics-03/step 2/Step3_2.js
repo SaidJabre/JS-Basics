60f32fd809bfc5bd1b3cb3ffb1363dd03f946edb
said jabre <saidjabre@gmail.com>
2022-01-24 21:33:03 +0200

    Step3_2

---
+++ javascript-basics-03/step 2/Step3_2.js
@@ -0,0 +1,7 @@
+//document.querySelector("#texte").style.display = "none";
+function showDV() {
+    document.querySelector("#texte").style.display = "block";
+}
+function hideDV() {
+    document.querySelector("#texte").style.display = "none";
+}
