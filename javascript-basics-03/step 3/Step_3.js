ab303c16cc8bc556ffbd8b45f57c18ee9f35f0d1
said jabre <saidjabre@gmail.com>
2022-01-24 23:46:19 +0200

    Step3_3b

--- javascript-basics-03/step 3/Step_3.js
+++ javascript-basics-03/step 3/Step_3.js
@@ -1,6 +1,6 @@
 document.querySelector(".blue").addEventListener("click",function () {
                                     changeColor("blue"); 
-                                    } );
+                                    }, false);
 
 function changeColor (color) {
     document.querySelector("#text").style.color = color;
